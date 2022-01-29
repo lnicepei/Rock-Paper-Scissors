ee445796c6829a9f29cff7b2d99e30295cd0569f
lnicepei <dmitrii.shaidorov@gmail.com>
2022-01-29 15:06:03 +0300

    finally i did it, but i gotta add something else xd

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 let computerSelection, playerSelection, computerScore = 0, playerScore = 0, restartBtn;
+
 restartBtn = document.createElement("button");
 restartBtn.innerHTML = "RESTART THE GAME";
 restartBtn.setAttribute("id", "restartbutton");
-
-
 
 const playerScoreInsideScore = document.createElement('div');
 playerScoreInsideScore.setAttribute("id", "playerscore");
 
 const computerScoreInsideScore = document.createElement('div');
 computerScoreInsideScore.setAttribute("id", "computerscore");
-
-// const scoreDiv = document.createElement('div');
-// computerScoreInsideScore.setAttribute("id", "score");
-// document.body.appendChild(scoreDiv);
 
 playerPlay();
 
@@ -32,52 +27,32 @@ function playerPlay(){
     const scoreContainer = document.createElement('div');
     scoreContainer.setAttribute("id", "score");
     document.body.appendChild(scoreContainer);
+
     document.getElementById('score').appendChild(computerScoreInsideScore);
     document.getElementById('score').appendChild(playerScoreInsideScore);
+
     document.getElementById('playerscore').textContent = "Player score is " + playerScore;
     document.getElementById('computerscore').textContent = "Computer score is " + computerScore;
-    document.getElementById('btnRock').addEventListener('click', function(){
-        playerSelection = "rock";
-        computerSelection = computerPlay();
-        playRound(computerSelection, playerSelection);
-        return;
-    });
-    document.getElementById('btnPaper').addEventListener('click', function(){
-        playerSelection = "paper";
-        computerSelection = computerPlay();
-        playRound(computerSelection, playerSelection);
-        return;
-    });
-    document.getElementById('btnScissors').addEventListener('click', function(){
-        playerSelection = "scissors";
-        computerSelection = computerPlay();
-        playRound(computerSelection, playerSelection);
-        return;
-    });   
+
+    document.getElementById('btnRock').addEventListener('click', remover1);
+    document.getElementById('btnPaper').addEventListener('click', remover2);
+    document.getElementById('btnScissors').addEventListener('click', remover3);   
+    
 }
 
 function restartGame(){
     document.getElementById('score').appendChild(restartBtn);
-    document.getElementById('restartbutton').addEventListener('click', function(){
+    document.getElementById('restartbutton').addEventListener('click', function remover(){
         playerScore = 0;
         computerScore = 0;
-        //document.getElementById('score').appendChild(restartBtn);
+        
         document.getElementById('score').removeChild(restartBtn);
         document.getElementById('score').textContent = "";
-        document.getElementById('score').remove();
-        // computerSelection = "";
-        // playerSelection = "";
-        // document.getElementById('score').remove();
-        // document.getElementById('btnScissors').removeEventListener('click', restartGame);
-        // document.getElementById('btnRock').removeEventListener('click', restartGame);
-        // document.getElementById('btnPaper').removeEventListener('click', restartGame);
-        // document.getElementById('playerscore').remove();
-        // document.getElementById('computerscore').remove();   
-        // document.getElementById('score').removeChild(computerScoreInsideScore);
-        // document.getElementById('score').removeChild(playerScoreInsideScore);
-        document.body.remove(scoreContainer);
-        document.body.appendChild(scoreContainer);
-        // document.getElementById('computerscore').textContent.remove();
+        
+        document.getElementById('btnScissors').removeEventListener('click', remover1);
+        document.getElementById('btnRock').removeEventListener('click', remover2);
+        document.getElementById('btnPaper').removeEventListener('click', remover3);
+
         playerPlay();
     });
 }
@@ -97,18 +72,39 @@ function playRound(computerSelection, playerSelection){
     if(computerScore < 3 && playerScore < 3){
         console.log("player score is " + playerScore);
         console.log("computer score is " + computerScore);
+
         document.getElementById('playerscore').textContent = "Player score is " + playerScore;
         document.getElementById('computerscore').textContent = "Computer score is " + computerScore;
+
     }else if(computerScore == 3){
-        playerScore = 0;
-        computerScore = 0;
         document.getElementById('score').textContent = "COMPUTER WINS";
+
         restartGame();
     }else if(playerScore == 3){
-        playerScore = 0;
-        computerScore = 0;
         document.getElementById('score').textContent = "PLAYER WINS";
+
         restartGame();
     }
+}
+
+function remover1(){
+    playerSelection = "rock";
+    computerSelection = computerPlay();
+    playRound(computerSelection, playerSelection);
+    return;
+}
+
+function remover2(){
+    playerSelection = "paper";
+    computerSelection = computerPlay();
+    playRound(computerSelection, playerSelection);
+    return;
+}
+
+function remover3(){
+    playerSelection = "scissors";
+    computerSelection = computerPlay();
+    playRound(computerSelection, playerSelection);
+    return;
 }
 
