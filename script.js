679b66c294d77649f3f1fd3b76cc6d85ebb5db84
lnicepei <dmitrii.shaidorov@gmail.com>
2022-01-30 12:17:22 +0300

    change buttons and make 'em unselectable

--- script.js
+++ script.js
@@ -3,21 +3,24 @@ let computerSelection, playerSelection, computerScore = 0, playerScore = 0, rest
 restartBtn = document.createElement("div");
 restartBtn.innerHTML = "RESTART THE GAME";
 restartBtn.setAttribute("id", "restartbutton");
-// document.restartBtn.style.color = "red";
 
 let playerScoreInsideScore = document.createElement('div');
 playerScoreInsideScore.setAttribute("id", "playerscore");
-// document.playerScoreInsideScore.style.cssText = "display: flex; justify-content: center; align-items: center; flex-direction: column; margin-bottom: 20px; background-color: yellow;";
 
 let computerScoreInsideScore = document.createElement('div');
 computerScoreInsideScore.setAttribute("id", "computerscore");
-document.getElementById('btnScissors').style.cssText = "padding: 10px;  border-radius: 20px; border: 2px solid black; box-shadow: 15px 10px 10px grey;"
-document.getElementById('btnRock').style.cssText = "padding: 10px;  border-radius: 20px; border: 2px solid black; box-shadow: 15px 10px 10px grey;"
-document.getElementById('btnPaper').style.cssText = "padding: 10px;  border-radius: 20px; border: 2px solid black; box-shadow: 15px 10px 10px grey;"
-document.getElementById('score').style.cssText = "display: flex; justify-content: center; align-items: center; flex-direction: column; padding: 10px;  border-radius: 20px; border: 2px solid black; box-shadow: 15px 10px 10px grey; margin: 50px"
+document.getElementById('btnScissors').style.cssText = "padding: 10px;  border-radius: 5px; border: 1px solid grey; box-shadow: 0 1px 1px rgba(0,0,0,0.08), 0 2px 2px rgba(0,0,0,0.12), 0 4px 4px rgba(0,0,0,0.16), 0 8px 8px rgba(0,0,0,0.20);	-webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; "
+document.getElementById('btnRock').style.cssText = "padding: 10px;  border-radius: 5px; border: 1px solid grey; box-shadow: 0 1px 1px rgba(0,0,0,0.08), 0 2px 2px rgba(0,0,0,0.12), 0 4px 4px rgba(0,0,0,0.16), 0 8px 8px rgba(0,0,0,0.20); -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; "
+document.getElementById('btnPaper').style.cssText = "padding: 10px;  border-radius: 5px; border: 1px solid grey; box-shadow: 0 1px 1px rgba(0,0,0,0.08), 0 2px 2px rgba(0,0,0,0.12), 0 4px 4px rgba(0,0,0,0.16), 0 8px 8px rgba(0,0,0,0.20); -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; "
+document.getElementById('score').style.cssText = "display: flex; justify-content: center; align-items: center; flex-direction: column; padding: 10px;  border-radius: 5px; border: 1px solid grey; box-shadow: 0 1px 1px rgba(0,0,0,0.08), 0 2px 2px rgba(0,0,0,0.12), 0 4px 4px rgba(0,0,0,0.16), 0 8px 8px rgba(0,0,0,0.20); margin: 50px"
+
+let resultsOfTheRound = document.createElement('div');
+resultsOfTheRound.setAttribute("id", "results");
+
 playerPlay();
 
 function computerPlay(){
+
     let x = Math.floor(Math.random() * 3);
     if(x == 0){
         return("rock");
@@ -26,30 +29,27 @@ function computerPlay(){
     }else{
         return("scissors");
     }
+
 }
 
 function playerPlay(){
-    // const scoreContainer = document.createElement('div');
-    // scoreContainer.setAttribute("id", "score");
-    // document.body.appendChild(scoreContainer);
 
     document.getElementById('score').appendChild(computerScoreInsideScore);
     document.getElementById('score').appendChild(playerScoreInsideScore);
-    // document.getElementById('score').cssText = "display: flex; justify-content: center;"
 
     document.getElementById('playerscore').textContent = "Player score is " + playerScore;
     document.getElementById('computerscore').textContent = "Computer score is " + computerScore;
 
     document.getElementById('btnRock').addEventListener('click', remover1);
     document.getElementById('btnPaper').addEventListener('click', remover2);
-    document.getElementById('btnScissors').addEventListener('click', remover3);   
+    document.getElementById('btnScissors').addEventListener('click', remover3); 
     
 }
 
 function restartGame(){
+
     document.getElementById('score').appendChild(restartBtn);
-    document.getElementById('score').style.cssText = "display: flex; justify-content: center; align-items: center; flex-direction: column; padding: 10px;  border-radius: 20px; border: 2px solid black; box-shadow: 15px 10px 10px grey; margin: 50px";
-    // document.getElementById('restartbutton').style.cssText = "display: flex; justify-content: center; align-items: center; background-color: yellow;";
+    document.getElementById('score').style.cssText = "display: flex; justify-content: center; align-items: center; flex-direction: column; padding: 10px;  border-radius: 5px; border: 1px solid grey; box-shadow: 0 1px 1px rgba(0,0,0,0.08), 0 2px 2px rgba(0,0,0,0.12), 0 4px 4px rgba(0,0,0,0.16), 0 8px 8px rgba(0,0,0,0.20); margin: 50px; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none;";
     document.getElementById('restartbutton').addEventListener('click', function remover(){
         
         playerScore = 0;
@@ -64,23 +64,25 @@ function restartGame(){
 
         playerPlay();
     });
+
 }
 
 function playRound(computerSelection, playerSelection){
+
     if(computerSelection == "rock" && playerSelection == "rock" ||  computerSelection == "paper" && playerSelection == "paper"  || computerSelection == "scissors" && playerSelection == "scissors"){
-        alert("tie!");
+        resultsOfTheRound.textContent = "tie";
+        document.getElementById('score').appendChild(resultsOfTheRound);
     }else if(computerSelection == "rock" && playerSelection == "paper" || computerSelection == "scissors" && playerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper"){
-        alert("player wins!");
+        resultsOfTheRound.textContent = "player wins";
+        document.getElementById('score').appendChild(resultsOfTheRound);
         playerScore++;
     }else if (computerSelection == "paper" && playerSelection == "rock" || computerSelection == "scissors" && playerSelection == "paper" || computerSelection == "rock" && playerSelection == "scissors"){
-        alert("computer wins!");
+        resultsOfTheRound.textContent = "computer wins";
+        document.getElementById('score').appendChild(resultsOfTheRound);
         computerScore++;
-    }else{
-        alert("Enter correct choice!");
     }
+
     if(computerScore < 3 && playerScore < 3){
-        console.log("player score is " + playerScore);
-        console.log("computer score is " + computerScore);
 
         document.getElementById('playerscore').textContent = "Player score is " + playerScore;
         document.getElementById('computerscore').textContent = "Computer score is " + computerScore;
@@ -94,25 +96,32 @@ function playRound(computerSelection, playerSelection){
 
         restartGame();
     }
+
 }
 
 function remover1(){
+
     playerSelection = "rock";
     computerSelection = computerPlay();
     playRound(computerSelection, playerSelection);
     return;
+
 }
 
 function remover2(){
+
     playerSelection = "paper";
     computerSelection = computerPlay();
     playRound(computerSelection, playerSelection);
     return;
+
 }
 
 function remover3(){
+
     playerSelection = "scissors";
     computerSelection = computerPlay();
     playRound(computerSelection, playerSelection);
     return;
+
 }
